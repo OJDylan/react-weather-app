5b47f4c1491c1e55cd94730757469ab85c4d163d
OJDylan <dylan@tngdigital.com.my>
2022-01-19 11:32:13 +0800

    changed import order

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-import CountrySearch from "./CountrySearch";
 import WeatherInfo from "./WeatherInfo";
+import CountrySearch from "./CountrySearch";
 
 const apiKey = {
   key: "2255f5b5834362289b68c776ffeefaae",
